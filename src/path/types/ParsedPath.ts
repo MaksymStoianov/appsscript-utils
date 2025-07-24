5fd16cdb38c551376232900a8be4ea2db556128d
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-24 13:06:53 +0200

    add: path/types

---
+++ src/path/types/ParsedPath.ts
@@ -0,0 +1,30 @@
+/**
+ * @since   0.1.0
+ * @version 0.1.0
+ */
+export interface ParsedPath {
+  /**
+   * The root of the url such as '/'.
+   */
+  root?: string | undefined;
+
+  /**
+   * The full directory url such as '/home/user/dir'.
+   */
+  dir?: string | undefined;
+
+  /**
+   * The filename including extension (if any) such as 'index.html'.
+   */
+  base?: string | undefined;
+
+  /**
+   * The file extension (if any) such as '.html'.
+   */
+  ext?: string | undefined;
+
+  /**
+   * The filename without extension (if any) such as 'index'.
+   */
+  name?: string | undefined;
+}
