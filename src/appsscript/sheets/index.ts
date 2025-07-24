9ff862dd64a78625f7cbae41fac8dca8cb651621
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-24 14:03:23 +0200

    add: src/appsscript/sheets/getColumnLetterByIndex.ts

--- src/appsscript/sheets/index.ts
+++ src/appsscript/sheets/index.ts
@@ -2,3 +2,4 @@ export * from "./types";
 
 export { doGridRangesIntersect } from "./doGridRangesIntersect";
 export { getColumnIndexByLetter } from "./getColumnIndexByLetter";
+export { getColumnLetterByIndex } from "./getColumnLetterByIndex";
