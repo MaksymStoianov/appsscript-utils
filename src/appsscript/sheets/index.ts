b939a2a43c76047eb55d8aec5d1311079549e659
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-24 13:35:38 +0200

    add: src/appsscript/sheets/types

---
+++ src/appsscript/sheets/index.ts
@@ -0,0 +1,18 @@
+export * from "./types";
+
+export { doGridRangesIntersect } from "./doGridRangesIntersect";
+export { getColumnIndexByLetter } from "./getColumnIndexByLetter";
+export { getColumnLetterByIndex } from "./getColumnLetterByIndex";
+export { getSheetById } from "./getSheetById";
+export { isCellGridRange } from "./isCellGridRange";
+export { isGridRangeContainedIn } from "./isGridRangeContainedIn";
+export { isGridRangeSameDimensions } from "./isGridRangeSameDimensions";
+export { isRange } from "./isRange";
+export { isRichTextValue } from "./isRichTextValue";
+export { isSheet } from "./isSheet";
+export { isSpreadsheet } from "./isSpreadsheet";
+export { isTextStyle } from "./isTextStyle";
+export { isValidSheetName } from "./isValidSheetName";
+export { isValidSpreadsheetId } from "./isValidSpreadsheetId";
+export { parseA1Notation } from "./parseA1Notation";
+export { toA1Notation } from "./toA1Notation";
