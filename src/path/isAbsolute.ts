94519be35c272755f1a5ce5dda7cdc0f273b222f
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-24 13:12:06 +0200

    add: path/isAbsolute.ts

---
+++ src/path/isAbsolute.ts
@@ -0,0 +1,21 @@
+import { EmptyStringException } from "../exceptions";
+import { isEmpty } from "../isEmpty";
+import { isString } from "../isString";
+
+/**
+ * Checks if a given string represents an absolute path or URL.
+ *
+ * @param   path - The string path or URL to check.
+ * @returns `true` if the path is absolute; otherwise, `false`.
+ */
+export function isAbsolute(path: string): boolean {
+  if (!isString(path) || isEmpty(path)) {
+    throw new EmptyStringException();
+  }
+
+  if (/^(\w+):\/\//.test(path) || /^(\w+):/.test(path)) {
+    return true;
+  }
+
+  return path.startsWith("/");
+}
