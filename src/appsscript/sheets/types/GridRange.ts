894b84aed0cd9a13b8b220e4f3005688fa4127eb
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-24 13:35:08 +0200

    add: src/appsscript/sheets/types

---
+++ src/appsscript/sheets/types/GridRange.ts
@@ -0,0 +1,42 @@
+/**
+ * A range on a sheets.
+ *
+ * @since   0.1.0
+ * @version 0.1.0
+ */
+export interface GridRange {
+  /**
+   * The sheets this range is on.
+   */
+  sheetId?: number | null;
+
+  /**
+   * The name of the sheet this range is on.
+   */
+  sheetName?: string | null;
+
+  /**
+   *
+   */
+  a1Notation?: string | null;
+
+  /**
+   * The start row (inclusive) of the range, or not set if unbounded.
+   */
+  startRowIndex?: number | null;
+
+  /**
+   * The end row (exclusive) of the range, or not set if unbounded.
+   */
+  endRowIndex?: number | null;
+
+  /**
+   * The start column (inclusive) of the range, or not set if unbounded.
+   */
+  startColumnIndex?: number | null;
+
+  /**
+   * The end column (exclusive) of the range, or not set if unbounded.
+   */
+  endColumnIndex?: number | null;
+}
