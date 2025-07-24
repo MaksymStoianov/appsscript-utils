53402683ea1b881bcabce04d07154214b8d5c8fc
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-24 13:31:53 +0200

    update: path

--- src/path/isAbsolute.ts
+++ src/path/isAbsolute.ts
@@ -5,10 +5,11 @@ import { isString } from "../isString";
 /**
  * Checks if a given string represents an absolute path or URL.
  *
- * @param   path - The string path or URL to check.
- * @returns `true` if the path is absolute; otherwise, `false`.
+ * @param        path - The string path or URL to check.
+ * @returns     `true` if the path is absolute; otherwise, `false`.
+ * @environment `Google Apps Script`, `Browser`
  */
-export function isAbsolute(path: string): boolean {
+export function isAbsolute(path: string): path is string {
   if (!isString(path) || isEmpty(path)) {
     throw new EmptyStringException();
   }
--- src/path/isRelative.ts
+++ src/path/isRelative.ts
@@ -3,8 +3,9 @@ import { isAbsolute } from "./isAbsolute";
 /**
  * Checks if a given string represents a relative path.
  *
- * @param   path - The string path to check.
- * @returns `true` if the path is relative; otherwise, `false`.
+ * @param       path - The string path to check.
+ * @returns     `true` if the path is relative; otherwise, `false`.
+ * @environment `Google Apps Script`, `Browser`
  */
 export function isRelative(path: string): path is string {
   return !isAbsolute(path);
