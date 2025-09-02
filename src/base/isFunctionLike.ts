965d78e5f86cf19bd062af2b721a5d72064f8263
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-09-02 18:27:10 +0200

    refactor(base/isObject): Use nonNil utility to improve readability of isObject function

---
+++ src/base/isFunctionLike.ts
@@ -0,0 +1,27 @@
+import { isObject } from "./isObject";
+import { toString } from "./toString";
+import { ObjectTag } from "./types";
+
+/**
+ * Checks if the provided value is a function.
+ *
+ * @param   value - The value to check.
+ * @returns `true` if the value is a function; otherwise, `false`.
+ * @since   0.1.0
+ * @version 0.1.0
+ */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export function isFunction(value: unknown): value is Function {
+  if (!isObject(value)) {
+    return false;
+  }
+
+  const tag = toString(value) as ObjectTag;
+
+  return [
+    ObjectTag.FUNCTION,
+    ObjectTag.GENERATOR_FUNCTION,
+    ObjectTag.ASYNC_FUNCTION,
+    ObjectTag.PROXY
+  ].includes(tag);
+}
